3dbecbf75688bf66dca74811c07a9af4049d016f
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2024-04-14 22:10:59 -0300

    Add seeder with users for clients and employess

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Empleado;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +22,26 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::truncate();
+        Empleado::truncate();
+
+        User::create([
+            'name' => 'Juan Perez',
+            'email' => 'juanperez@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',  //password
+            // 'password' => '$2a$12$BSycJGxQJUmz2rSyn9Wv2.BpmKRV3Fjfw2DzEPHlKF4dDULvUjiPS', //123
+            'remember_token' => Str::random(10),
+        ]);
+
+        Empleado::create([
+            'name' => 'admin',
+            'email' => 'admin@jumping.com',
+            // 'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',  //password
+            // 'password' => '$2a$12$BSycJGxQJUmz2rSyn9Wv2.BpmKRV3Fjfw2DzEPHlKF4dDULvUjiPS', //123
+            'remember_token' => Str::random(10),
+        ]);
         $this->call(Talles::class);
         $this->call(TipoArticulos::class);
         $this->call(Equipos::class);;
