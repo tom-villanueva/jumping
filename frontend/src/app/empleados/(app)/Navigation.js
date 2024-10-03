3056c35872fde8db1142d0fc246f5cb90ae42b48
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2024-10-03 14:45:53 -0300

    Replaced svg

--- frontend/src/app/empleados/(app)/Navigation.js
+++ frontend/src/app/empleados/(app)/Navigation.js
@@ -8,6 +8,7 @@ import ResponsiveNavLink, {
 import { DropdownButton } from '@/components/DropdownLink'
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
+import { ChevronDown, MenuIcon, User } from 'lucide-react'
 
 const routes = [
   { id: 1, name: 'dashboard' },
@@ -27,7 +28,7 @@ const Navigation = ({ user, logout }) => {
   const pathname = usePathname()
 
   return (
-    <nav className="overflow-x-auto overflow-y-hidden border-b border-gray-100 bg-slate-700">
+    <nav className="border-b border-gray-100 bg-slate-700">
       {/* Primary Navigation Menu */}
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 justify-between">
@@ -62,21 +63,12 @@ const Navigation = ({ user, logout }) => {
                   <div>{user?.name}</div>
 
                   <div className="ml-1">
-                    <svg
-                      className="h-4 w-4 fill-current"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20">
-                      <path
-                        fillRule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <ChevronDown className="w-4" />
                   </div>
                 </button>
               }>
               {/* Authentication */}
-              <DropdownButton onClick={logout}>Logout</DropdownButton>
+              <DropdownButton onClick={logout}>Cerrar Sesión</DropdownButton>
             </Dropdown>
           </div>
 
@@ -85,29 +77,7 @@ const Navigation = ({ user, logout }) => {
             <button
               onClick={() => setOpen(open => !open)}
               className="inline-flex items-center justify-center rounded-md p-2 text-black transition duration-150 ease-in-out hover:bg-gray-100 hover:text-gray-500 focus:bg-gray-100 focus:text-gray-500 focus:outline-none">
-              <svg
-                className="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24">
-                {open ? (
-                  <paths
-                    className="inline-flex"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    className="inline-flex"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
-              </svg>
+              <MenuIcon className="w-4" />
             </button>
           </div>
         </div>
@@ -131,19 +101,7 @@ const Navigation = ({ user, logout }) => {
           <div className="border-t border-gray-200 pb-1 pt-4">
             <div className="flex items-center px-4">
               <div className="flex-shrink-0">
-                <svg
-                  className="h-10 w-10 fill-current text-slate-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
+                <User className="w-8" />
               </div>
 
               <div className="ml-3">
@@ -158,7 +116,9 @@ const Navigation = ({ user, logout }) => {
 
             <div className="mt-3 space-y-1">
               {/* Authentication */}
-              <ResponsiveNavButton onClick={logout}>Logout</ResponsiveNavButton>
+              <ResponsiveNavButton onClick={logout}>
+                Cerrar Sesión
+              </ResponsiveNavButton>
             </div>
           </div>
         </div>
--- frontend/src/components/DropdownLink.js
+++ frontend/src/components/DropdownLink.js
@@ -2,31 +2,31 @@ import Link from 'next/link'
 import { Menu } from '@headlessui/react'
 
 const DropdownLink = ({ children, ...props }) => (
-    <Menu.Item>
-        {({ active }) => (
-            <Link
-                {...props}
-                className={`w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 ${
-                    active ? 'bg-gray-100' : ''
-                } focus:outline-none transition duration-150 ease-in-out`}>
-                {children}
-            </Link>
-        )}
-    </Menu.Item>
+  <Menu.Item>
+    {({ active }) => (
+      <Link
+        {...props}
+        className={`block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 ${
+          active ? 'bg-gray-100' : ''
+        } transition duration-150 ease-in-out focus:outline-none`}>
+        {children}
+      </Link>
+    )}
+  </Menu.Item>
 )
 
 export const DropdownButton = ({ children, ...props }) => (
-    <Menu.Item>
-        {({ active }) => (
-            <button
-                className={`w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 ${
-                    active ? 'bg-gray-100' : ''
-                } focus:outline-none transition duration-150 ease-in-out`}
-                {...props}>
-                {children}
-            </button>
-        )}
-    </Menu.Item>
+  <Menu.Item>
+    {({ active }) => (
+      <button
+        className={`block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 ${
+          active ? 'bg-gray-100' : ''
+        } transition duration-150 ease-in-out focus:outline-none`}
+        {...props}>
+        {children}
+      </button>
+    )}
+  </Menu.Item>
 )
 
 export default DropdownLink
