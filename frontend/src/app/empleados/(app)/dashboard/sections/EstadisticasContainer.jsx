e4f2c182fff97c245248cd52b6c812339fc761de
tom-villanueva <tomas.villanueva.jousset@gmail.com>
2024-10-16 11:11:11 -0300

    Cambiar label a gráfico

--- frontend/src/app/empleados/(app)/dashboard/sections/EstadisticasContainer.jsx
+++ frontend/src/app/empleados/(app)/dashboard/sections/EstadisticasContainer.jsx
@@ -69,7 +69,7 @@ export default function EstadisticasContainer() {
         />
       </div>
       <div className="col-span-3 flex flex-col gap-8 rounded-lg border border-slate-400 bg-slate-700 p-3">
-        <p className="ml-5 text-xl font-bold text-white">Reservas</p>
+        <p className="ml-5 text-xl font-bold text-white">Ingresos por año</p>
         <InfoChart data={estadisticas?.monthly_estadisticas} />
       </div>
     </>
