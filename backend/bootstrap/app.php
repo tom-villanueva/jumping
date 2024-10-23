a4be03b8d299b4825c43bd499236b12641d9a9a8
Tomás Villanueva <tomas.villanueva.jousset@gmail.com>
2024-10-23 04:41:32 +0000

    PROD funcionando

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -25,21 +26,33 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-            if ($request->is('api/*')) { 
-                $id = $request->route(('id'));
-                if($id != null) {
-                    $type = explode('/', $request->path())[1]; 
-                    return response()->json([
-                        'message' => 'No se encuentra el registro.', 
-                        'errors' => ["not found" => ["No se encuentra el registro en $type para el id $id"]]
-                    ], 404);
-                } else {
-                    return response()->json([
-                        'message' => 'No se encuentra la ruta.', 
-                        'errors' => ["not found" => ["No se encuentra la ruta."]]
-                    ], 404);
-                }
+        // Log all exceptions to a file in storage/logs
+    $exceptions->render(function (\Exception $e, Request $request) {
+        // Log exception details to a file
+        Log::channel('daily')->error($e->getMessage(), [
+            'exception' => $e,
+            'url' => $request->url(),
+            'input' => $request->all()
+        ]);
+
+        // Handle specific exceptions
+        if ($e instanceof NotFoundHttpException && $request->is('api/*')) {
+            $id = $request->route('id');
+            if ($id != null) {
+                $type = explode('/', $request->path())[1];
+                return response()->json([
+                    'message' => 'No se encuentra el registro.',
+                    'errors' => ["not found" => ["No se encuentra el registro en $type para el id $id"]]
+                ], 404);
+            } else {
+                return response()->json([
+                    'message' => 'No se encuentra la ruta.',
+                    'errors' => ["not found" => ["No se encuentra la ruta."]]
+                ], 404);
             }
-        });
+        }
+
+        // Return the default exception response for other exceptions
+        return null;
+    });
     })->create();
